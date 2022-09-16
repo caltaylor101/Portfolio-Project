2b9d60ad37e5861b17c7e3cce7013c070868d0b9
caltaylor <Caltaylor101@gmail.com>
2022-09-16 12:59:22 -0600

    Starting

---
+++ Domain/Blog.cs
@@ -0,0 +1,13 @@
+
+namespace Domain
+{
+    public class Blog
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Body { get; set; }
+    }
+}

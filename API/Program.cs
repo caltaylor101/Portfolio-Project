f6e6fa733c3a59750a74a53011da5197251717b7
caltaylor <Caltaylor101@gmail.com>
2022-09-16 19:46:07 -0600

    Adding React and UIs

--- API/Program.cs
+++ API/Program.cs
@@ -14,11 +14,21 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod(). AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
 
 var services = scope.ServiceProvider;
+
+
 
 try
 {
@@ -38,7 +48,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/blog').then(response => {
+      console.log(response);
+      setBlogs(response.data);
+    });
+  }, []);
+
+
+  return (
+    <div className="App">
+      <ul>
+        {blogs.map((blog: any) => (
+          <li key={blog.id}>
+              {blog.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;

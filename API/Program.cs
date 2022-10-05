ecc1cb890b8b2e1ee9268cb74d52eab43cfb1968
caltaylor <Caltaylor101@gmail.com>
2022-10-04 22:57:38 -0600

    Added Identity

--- API/Program.cs
+++ API/Program.cs
@@ -26,6 +26,9 @@ builder.Services.AddFluentValidationAutoValidation();
 // builder.Services.AddFluentValidationClientsideAdapters();
 builder.Services.AddValidatorsFromAssemblyContaining<BlogCreate>();
 
+builder.Services.AddIdentityServices(builder.Configuration);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
+
         }
 
         public DbSet<Blog> Blogs { get; set; }

1ed625d6c6bf88daf2702108df935f765b49fffa
caltaylor <Caltaylor101@gmail.com>
2022-10-19 01:12:02 -0600

    Mapped user photos to their profile and finished photos controller isMain method.

--- API/Controllers/PhotoController.cs
+++ API/Controllers/PhotoController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class PhotosController : BaseApiController
+    public class PhotoController : BaseApiController
     {
         [HttpPost]
         public async Task<IActionResult> Add([FromForm] AddPhoto.Command command)
@@ -19,6 +19,12 @@ namespace API.Controllers
         public async Task<IActionResult> Delete(string id)
         {
             return HandleResult(await Mediator.Send(new DeletePhoto.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/setMainPhoto")]
+        public async Task<IActionResult> SetMainPhoto(string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMainPhoto.Command{Id = id}));
         }
 
     }
--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -9,9 +9,9 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Blog, Blog>();
-
-
             CreateMap<Blog, BlogDto>();
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(i => i.Image, o => o.MapFrom(p => p.Photos.FirstOrDefault(x => x.IsMainProfilePicture).Url));
         }
     }
 }

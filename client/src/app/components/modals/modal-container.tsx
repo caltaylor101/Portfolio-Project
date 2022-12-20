0d1cc648f4848e4a54e4cc4123d7a7b6a31449f0
caltaylor <Caltaylor101@gmail.com>
2022-12-20 15:12:57 -0700

    Update modal-container.tsx
    Reformat

--- client/src/app/components/modals/modal-container.tsx
+++ client/src/app/components/modals/modal-container.tsx
@@ -6,8 +6,8 @@ import { useStore } from "../../stores/store";
 export default observer(function ModalContainer() {
     const { modalStore } = useStore();
     return (
-    <Modal style={{backgroundColor: '#333333 !important'}} open={modalStore.modal.open} onCancel={modalStore.closeModal} centered footer={null}>
-                {modalStore.modal.body}
+        <Modal style={{ backgroundColor: '#333333 !important' }} open={modalStore.modal.open} onCancel={modalStore.closeModal} centered footer={null}>
+            {modalStore.modal.body}
         </Modal>
     )
 })
